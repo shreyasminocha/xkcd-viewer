e1af0a82f5394070ecf7c6bd733d4bef63a2e57d
Shreyas Minocha <shreyasminocha7@gmail.com>
2018-04-08 21:39:33 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const request = require('request');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'static')));
+
+// routes
+app.get('/:comic?', (req, res, next) => {
+    const number = req.params.comic || '';
+
+    request(`https://www.xkcd.com/${number}/info.0.json`, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+            res.render('comic', JSON.parse(body));
+        } else {
+            next();
+        }
+    });
+
+});
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.locals.title = err.message;
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
---
+++ readme.md
@@ -0,0 +1,13 @@
+# xkcd-viewer
+
+I did this project to get some exposure to the [express framework](//expressjs.com).
+
+## Features
+
+- Alt/title text below the image.
+- Each comic links to its [explainxkcd](//explainxkcd.com) page.
+- Each comic has a download link.
+
+## License
+
+Licensed under the [MIT License](//shreyas.mit-license.org/2018).
